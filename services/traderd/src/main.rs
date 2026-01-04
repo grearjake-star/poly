63bd54d0398845f9df4406575811e3901231313c
grearjake-star <grear.jake@gmail.com>
2026-01-03 20:50:56 -0500

    Update main.rs

--- services/traderd/src/main.rs
+++ services/traderd/src/main.rs
@@ -299,24 +299,25 @@ mod tests {
 
     #[test]
     fn creates_parent_directory_for_windows_style_sqlite_url() {
-        let _guard = crate::cwd_guard().lock().expect("cwd guard should lock");
-        let tmp_dir = env::temp_dir().join(format!("poly_traderd_{}", Uuid::new_v4()));
-        fs::create_dir_all(&tmp_dir).expect("temp dir should be creatable");
+    let _guard = crate::cwd_guard().lock().expect("cwd guard should lock");
+    let tmp_dir = env::temp_dir().join(format!("poly_traderd_{}", Uuid::new_v4()));
+    fs::create_dir_all(&tmp_dir).expect("temp dir should be creatable");
 
-            let original_dir = env::current_dir().expect("current dir should be readable");
-            env::set_current_dir(&tmp_dir).expect("should be able to change to temp dir");
+    let original_dir = env::current_dir().expect("current dir should be readable");
+    env::set_current_dir(&tmp_dir).expect("should be able to change to temp dir");
 
-            ensure_sqlite_parent_dir(url).expect("should be able to create parent directories");
+    // ✅ Define `url` in scope
+    let url = "sqlite:///C:poly/data/traderd.sqlite";
+    ensure_sqlite_parent_dir(url).expect("should be able to create parent directories");
 
-        let expected_parent = tmp_dir.join("C:poly").join("data");
-        assert!(
-            expected_parent.is_dir(),
-            "expected parent directory {:?} to exist",
-            expected_parent
-        );
+    let expected_parent = tmp_dir.join("C:poly").join("data");
+    assert!(
+        expected_parent.is_dir(),
+        "expected parent directory {:?} to exist",
+        expected_parent
+    );
 
-            env::set_current_dir(original_dir).expect("should be able to restore cwd");
-        }
+    env::set_current_dir(original_dir).expect("should be able to restore cwd");
     }
 
     #[test]
